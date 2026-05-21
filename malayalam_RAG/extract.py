e42e6b3d715bfea9afa3834c0a063c6962b4f94c
Husaima Mailanchy T K <husaimamailanchytk@Husaimas-MacBook-Air.local>
2026-05-22 00:11:48 +0530

    Created chunks of the extracted text

--- malayalam_RAG/extract.py
+++ malayalam_RAG/extract.py
@@ -1,18 +1,41 @@
 
 import fitz
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def extract_text_from_pdf(file_path):
     print(f"Opening...{file_path}")
 
+    extracted_text = ""
+
     doc = fitz.open(file_path)
-    print(len(doc))
-    text = doc[1].get_text()
-    print(text)
+    for page_no in range(len(doc)):
+        page = doc[page_no]
+        extracted_text += page.get_text()
+
+    return extracted_text
+
+def chunk_text(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, 
+        chunk_overlap=100,
+        length_function=len
+        )
+    
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+
 
 if __name__ == '__main__':
     test_pdf = 'kerala.pdf'
 
     try:
-        extract_text_from_pdf(test_pdf)
+        result = extract_text_from_pdf(test_pdf)
+        print(f'Total characters extracted: {len(result)}')
+        chunks = chunk_text(result)
+        print(f'Total chunks created: {len(chunks)}')
+        print('Sample chunk:', chunks[0])
+
     except Exception as e:
         print(e)
