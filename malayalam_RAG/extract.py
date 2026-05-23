ad323fb52f21a8d437900054de58c8bdfa9be367
Husaima Mailanchy T K <husaimamailanchytk@Husaimas-MacBook-Air.local>
2026-05-23 17:26:09 +0530

    vectoized and created the database

--- malayalam_RAG/extract.py
+++ malayalam_RAG/extract.py
@@ -1,6 +1,11 @@
 
 import fitz
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+
+load_dotenv()
 
 def extract_text_from_pdf(file_path):
     print(f"Opening...{file_path}")
@@ -24,18 +29,35 @@ def chunk_text(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
+def vector_db(chunks):
+    embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+    print("Creating vector db...")
+    db = Chroma.from_texts(
+        texts = chunks,
+        embedding = embedding_model,
+        persist_directory="./kerala_db"
+    )
+    return db
 
-
+    
 
 if __name__ == '__main__':
     test_pdf = 'kerala.pdf'
 
     try:
+        #extract
         result = extract_text_from_pdf(test_pdf)
         print(f'Total characters extracted: {len(result)}')
+
+        #chunk
         chunks = chunk_text(result)
         print(f'Total chunks created: {len(chunks)}')
         print('Sample chunk:', chunks[0])
 
+        #vector_db
+        vector_database = vector_db(chunks)
+        total_vectors = vector_database._collection.count()
+        print(f'Total vectors in the database: {total_vectors}')
+        
     except Exception as e:
         print(e)
